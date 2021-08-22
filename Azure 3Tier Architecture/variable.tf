6c22ca02d4b1b471bca990b1e9edeb8c61d7fae9
PrasannaCloudPro <55081476+PrasannaCloudSME@users.noreply.github.com>
2021-08-22 08:33:58 +0530

    Add files via upload
    #3TierAzure

---
+++ Azure 3Tier Architecture/variable.tf
@@ -0,0 +1,56 @@
+variable "resource_group" {
+  
+  
+}
+variable "location" {
+    
+}
+variable "Vnetname" {
+  
+}
+variable "subnet1" {
+
+
+  
+}
+
+variable "subnet2" {
+
+
+  
+}
+
+variable "subnet3" {
+
+
+  
+}
+
+variable "Publicipweb" {
+  
+  
+}
+variable "Publicipapp" {
+  
+  
+}
+variable "PublicipDB" {
+  
+  
+}
+variable "loadbalancervarweb" {
+
+   
+
+}
+variable "loadbalancervarapp" {
+
+   
+
+}
+variable "loadbalancervarDB" {
+
+   
+
+}
+
