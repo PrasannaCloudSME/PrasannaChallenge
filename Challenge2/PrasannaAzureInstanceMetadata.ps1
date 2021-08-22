116a9a36305b7638f6a00a170ebecc8b4c842679
PrasannaCloudPro <55081476+PrasannaCloudSME@users.noreply.github.com>
2021-08-22 09:09:33 +0530

    Challenge2

---
+++ Challenge2/PrasannaAzureInstanceMetadata.ps1
@@ -0,0 +1,1 @@
+﻿Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json | Out-File -FilePath C:\Users\PrasannaAzureVminstance.json
